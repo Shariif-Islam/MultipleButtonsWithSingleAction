9d15143294a765be71c150334a908bb90adaa4c8
Shariif Islam <islamshariif@gmail.com>
2017-05-24 15:15:46 +0600

    Push to git

--- MultipleButtonsWithSingleAction/ViewController.swift
+++ MultipleButtonsWithSingleAction/ViewController.swift
@@ -10,16 +10,51 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var buttons: [UIButton]!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        customizeButton()
+    }
+    
+    func customizeButton(){
+        
+        for button in buttons {
+            
+            if button.tag == 102 {
+                
+                button.backgroundColor = UIColor.orange
+                button.setTitleColor(UIColor.white, for: .normal)
+            }
+            else {
+                
+                button.backgroundColor = UIColor.yellow
+                button.setTitleColor(UIColor.black, for: .normal)
+            }
+        }
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func buttonAction(_ sender: Any) {
+        
+       let button = sender as! UIButton
+        
+        /**
+         - we can identify button by tag name or button tilte text
+         */
+        
+        if button.titleLabel?.text == "One" {
+        
+            print("Button one pressed")
+        }
+        else if button.tag == 103 {
+        
+            print("Button three pressed")
+            
+        }
+        else {
+            print("Button two pressed")
+        }
     }
-
-
 }
 
